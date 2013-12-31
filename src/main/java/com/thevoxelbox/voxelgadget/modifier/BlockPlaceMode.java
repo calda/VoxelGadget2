25b2ec86d4849553452c3402c8d95a6faec975b7
CalDaBeast <calstephens98@gmail.com>
2013-12-30 20:32:30 -0500

    Fixed BlockPlaceMode bug which disabled the Override Modifier

--- src/main/java/com/thevoxelbox/voxelgadget/modifier/BlockPlaceMode.java
+++ src/main/java/com/thevoxelbox/voxelgadget/modifier/BlockPlaceMode.java
@@ -2,6 +2,7 @@ package com.thevoxelbox.voxelgadget.modifier;
 
 import com.thevoxelbox.voxelgadget.Processor;
 import org.bukkit.block.Block;
+import org.bukkit.inventory.ItemStack;
 
 public class BlockPlaceMode extends AbstractModeModifier {
 
@@ -13,7 +14,7 @@ public class BlockPlaceMode extends AbstractModeModifier {
 		}
 		Block existing = p.getTargetBlock();
 		if (p.getOverride() == null) setBlock(existing, p.getDispensed(), p.applyPhysics(), p);
-		else setBlock(existing, p.getDispensed(), p.applyPhysics(), p);
+		else setBlock(existing, new ItemStack(p.getOverride().getTypeId(), p.getDispensed().getAmount(), p.getOverride().getData() ), p.applyPhysics(), p);
 		return 0;
 	}
 
